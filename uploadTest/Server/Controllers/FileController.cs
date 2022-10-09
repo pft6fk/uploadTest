7e962a302f49ac5ed9f0c2b4353277e0d379d7ab
Muhammadaziz <juraev.mukhammadaziz@gmail.com>
2022-10-10 01:13:41 +0500

    Add project files.

---
+++ uploadTest/Server/Controllers/FileController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using uploadTest.Shared;
+
+namespace uploadTest.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public FileController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<UploadResult>>> UploadFile(List<IFormFile> files)
+        {
+            List<UploadResult> uploadResults = new();
+            foreach (var file in files)
+            {
+                var uploadResult = new UploadResult();
+                string trustedFileNameForFileStorage;
+                var untrustedFileName = file.Name;
+                //uploadResult.FileName = file.Name;
+                uploadResult.FileName = untrustedFileName;
+                //var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
+                var trustedFileNameForDisplay = WebUtility.HtmlEncode(file.Name);
+                trustedFileNameForFileStorage = file.FileName;
+                //trustedFileNameForFileStorage = Guid.NewGuid().ToString();
+                //trustedFileNameForFileStorage = Path.GetRandomFileName();
+
+                var path = Path.Combine(_env.ContentRootPath, "uploads", trustedFileNameForFileStorage);
+
+                await using FileStream fs = new FileStream(path, FileMode.Create);
+                await file.CopyToAsync(fs);
+
+                uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                uploadResults.Add(uploadResult);
+
+            }
+
+            return Ok(uploadResults);
+        }
+    }
+}
---
+++ uploadTest/Shared/UploadResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uploadTest.Shared
+{
+    public class UploadResult
+    {
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+    }
+    public class Element
+    {
+        public int Number { get; set; }
+        public string Sign { get; set; }
+        public string Name { get; set; }
+        public int Position { get; set; }
+        public double Molar { get; set; }
+    }
+}
